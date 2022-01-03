e929895046f213802f374ba39919e359ae601999
Maxim <maks.shherbako04@gmail.com>
2022-01-03 16:40:41 +0400

    add perfd.py

--- perfd.py
+++ perfd.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 import os
-command = "perf record -e cycles:u -j any,u -a -o perf.data -- sleep 10"
-os.system( command ) 
+#os.system("perf record -e cycles:u -j any,u -a -o perf.data -- sleep 10")  
+import subprocess
+batcmd="perf script -F ip,dso -f -i perf.data"
+result = subprocess.check_output(batcmd, shell=True, text =True)
+lines= result.splitlines()
+for line in lines:
+    print(line)
+print(line.split()) 
+
+#print( ", ".join( repr(e) for e in result ) ) #пытался удалить скобки
