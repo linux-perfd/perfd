b6945d5de46359cea70c8d1eb07c9008874c6ebb
Maxim <maks.shherbako04@gmail.com>
2022-01-09 11:20:43 +0400

    Count samples per object file

--- perfd.py
+++ perfd.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 import os
-command = "perf record -e cycles:u -j any,u -a -o perf.data -- sleep 10"
-os.system( command ) 
+os.system("perf record -e cycles:u -j any,u -a -o perf.data -- sleep 10")
+import subprocess
+batcmd="perf script -F ip,dso -f -i perf.data"
+result = subprocess.check_output(batcmd, shell=True, text =True)
+lines= result.splitlines()
+samples_per_obj_file = dict()
+
+for line in lines:
+    cols = line.split()
+    name_parens = cols[1]
+    name = name_parens[1:-1] 
+
+    if name in samples_per_obj_file:
+        samples_per_obj_file[name] += 1
+    else:
+        samples_per_obj_file[name] = 0
+for name in samples_per_obj_file:      
+    print(name, samples_per_obj_file[name])
