88e9ad404de40572393e259d32ed78041a7fe889
Maxim <maks.shherbako04@gmail.com>
2022-01-09 04:50:03 +0400

    Add perfd.py

--- perfd.py
+++ perfd.py
@@ -5,8 +5,10 @@ import subprocess
 batcmd="perf script -F ip,dso -f -i perf.data"
 result = subprocess.check_output(batcmd, shell=True, text =True)
 lines= result.splitlines()
+samples_per_obj_file = {}
+print(len(samples_per_obj_file))
 for line in lines:
-    print(line)
-print(line.split()) 
-
-#print( ", ".join( repr(e) for e in result ) ) #пытался удалить скобки
+    print(line.split()[1] [1:-1] ) 
+for i in lines:
+    samples_per_obj_file[i] = samples_per_obj_file.get(i , 0) + 1
+    print(samples_per_obj_file[i] - 1 , sep = '/n')
