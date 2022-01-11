e993bf9c5df052e2ce3709cb6a8556ff3223e819
Maxim <maks.shherbako04@gmail.com>
2022-01-12 00:09:43 +0400

    try to create the unique filename for perf file using tempfile

--- perfd.py
+++ perfd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
-os.system("perf record -e cycles:u -j any,u -a -o perf.data -- sleep 10")
+#os.system("perf record -e cycles:u -j any,u -a -o perf.data -- sleep 10")
 import subprocess
 batcmd="perf script -F ip,dso -f -i perf.data"
 result = subprocess.check_output(batcmd, shell=True, text =True)
@@ -18,3 +18,32 @@ for line in lines:
         samples_per_obj_file[name] = 0
 for name in samples_per_obj_file:      
     print(name, samples_per_obj_file[name])
+
+import tempfile
+
+
+
+
+from sql30 import db
+name_perf_file = NamedTemporaryFile(delete=False)
+name_perf_file.name 
+name_perf_file.read()
+
+class Perfd(db.Model):
+    TABLE = 'reviews'
+    PKEY = 'rid'
+    DB_SCHEMA = {
+        'db_name': './perfd.dp',
+        'tables': [
+            {
+                'name': TABLE,
+                'fields': {
+                    'perf_name': 'text',
+                    'name_obj_file': 'text',
+                    'amount_of_samples': 'int'
+                },
+            'primary_key': PKEY
+            }
+        ]
+    }
+    VALIDATE_WRITE = True
