82e91de8d83ae0b688a1f8b0b97a42e67ed1fbdf
Maxim <maks.shherbako04@gmail.com>
2022-01-14 00:56:10 +0400

    Created  db.py

--- perfd.py
+++ perfd.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 import os
-#os.system("perf record -e cycles:u -j any,u -a -o name_perf_file.name -- sleep 10")
+import tempfile
+
+name_perf_file = tempfile.TemporaryFile() 
+file = name_perf_file.name
+
+os.system("perf record -e cycles:u -j any,u -a -o file -- sleep 10")
+batcmd ="perf script -F ip,dso -f -i file"
 import subprocess
-batcmd ="perf script -F ip,dso -f -i name_perf_file.name"
 result = subprocess.check_output(batcmd, shell=True, text =True)
 lines = result.splitlines()
 samples_per_obj_file = dict()
@@ -12,40 +17,15 @@ for line in lines:
     name_parens = cols[1]
     name = name_parens[1:-1] 
  
-    if name in samples_per_obj_file:
-        samples_per_obj_file[name] += 1
-    else:
-        samples_per_obj_file[name] = 0
-for name in samples_per_obj_file:      
-    print(samples_per_obj_file[name], name)
+    #if name in samples_per_obj_file:
+    #    samples_per_obj_file[name] += 1
+   # else:
+ #       samples_per_obj_file[name] = 0
+#for name in samples_per_obj_file:      
+    #print(samples_per_obj_file[name], name)
 
-import tempfile
-name_perf_file = tempfile.TemporaryFile()
-name_perf_file.name 
-name_perf_file.read()
-
-
-#from db import perfd ( I haven't understood this line in the comment yet) 
-from sql30 import db
-class Perfd(db.Model):
-    TABLE = 'perfd_database'
-    DB_SCHEMA = {
-        'db_name': './perfd.db',
-        'tables': [
-            {
-                'name': TABLE,
-                'fields': {
-                    'perf_name': 'text',
-                    'name_obj_file': 'text',
-                    'amount_of_samples': 'int'
-                },
-            }
-        ]
-    }
-    VALIDATE_WRITE = True
-import os 
-import perfd  
-db.tbl = ' perfd_database '
-db = perfd.Perfd()
-db.write(perf_name = ' ', name_obj_file = ' ', amount_of_samples = )  #Tried to call up the information like this it's not correct though   
+db = Database()
+db.tbl = 'database'
+db.write(name) 
+db.commit() 
 db.read() 
