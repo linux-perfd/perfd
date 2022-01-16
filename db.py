12f6cd09c96d48aecc33801137d73c50f076dc9a
Maxim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2022-01-16 23:18:28 +0400

    Use sql30. Update the amount of samples if you rerun the command again  (#9)
    * try to create the unique filename for perf file using tempfile
    
    * Created  db.py

---
+++ db.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from sql30 import db
+
+class Database(db.Model):
+    TABLE = 'database'
+    DB_SCHEMA = {
+        'db_name': './perfd.db',
+        'tables': [
+            {
+                'name': TABLE,
+                'fields': {
+                    'perf_name': 'text',
+                    'name_obj_file': 'text',
+                    'amount_of_samples': 'int'
+                },
+            }
+        ]
+    }
+    VALIDATE_WRITE = True
--- perfd.py
+++ perfd.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 import os
-os.system("perf record -e cycles:u -j any,u -a -o perf.data -- sleep 10")
+import tempfile
 import subprocess
-batcmd="perf script -F ip,dso -f -i perf.data"
+import db
+
+temp_perf_file = tempfile.NamedTemporaryFile(delete= False) 
+#temp_perf_file.name
+
+os.system("perf record -e cycles:u -j any,u -a -o  " + temp_perf_file.name + "  -- sleep 10")
+batcmd ="perf script -F ip,dso -f -i " + temp_perf_file.name
+
 result = subprocess.check_output(batcmd, shell=True, text =True)
-lines= result.splitlines()
+lines = result.splitlines()
 samples_per_obj_file = dict()
 
 for line in lines:
@@ -15,6 +22,12 @@ for line in lines:
     if name in samples_per_obj_file:
         samples_per_obj_file[name] += 1
     else:
-        samples_per_obj_file[name] = 0
+        samples_per_obj_file[name] = 1
+
+database = db.Database()
+database.table = 'database'
 for name in samples_per_obj_file:      
-    print(name, samples_per_obj_file[name])
+    database.write(perf_name= temp_perf_file.name, name_obj_file= name, amount_of_samples= samples_per_obj_file[name])
+
+database.commit() 
+print(database.read()) 
