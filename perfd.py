68d22cfc31b26f6a3f60e24772d2663cccbf2407
Maxim <maks.shherbako04@gmail.com>
2022-01-18 23:11:33 +0400

    Lint sources with pylint

--- perfd.py
+++ perfd.py
@@ -4,20 +4,21 @@ import tempfile
 import subprocess
 import db
 
-temp_perf_file = tempfile.NamedTemporaryFile(delete= False) 
-#temp_perf_file.name
+temp_perf_file = tempfile.NamedTemporaryFile(delete=False)
+# temp_perf_file.name
 
-os.system("perf record -e cycles:u -j any,u -a -o  " + temp_perf_file.name + "  -- sleep 10")
-batcmd ="perf script -F ip,dso -f -i " + temp_perf_file.name
+os.system("perf record -e cycles:u -j any,u -a -o  " +
+          temp_perf_file.name + "  -- sleep 10")
+batcmd = "perf script -F ip,dso -f -i " + temp_perf_file.name
 
-result = subprocess.check_output(batcmd, shell=True, text =True)
+result = subprocess.check_output(batcmd, shell=True, text=True)
 lines = result.splitlines()
 samples_per_obj_file = dict()
 
 for line in lines:
     cols = line.split()
     name_parens = cols[1]
-    name = name_parens[1:-1] 
+    name = name_parens[1:-1]
 
     if name in samples_per_obj_file:
         samples_per_obj_file[name] += 1
@@ -26,8 +27,11 @@ for line in lines:
 
 database = db.Database()
 database.table = 'database'
-for name in samples_per_obj_file:      
-    database.write(perf_name= temp_perf_file.name, name_obj_file= name, amount_of_samples= samples_per_obj_file[name])
+for name in samples_per_obj_file:
+    database.write(
+        perf_name=temp_perf_file.name,
+        name_obj_file=name,
+        amount_of_samples=samples_per_obj_file[name])
 
-database.commit() 
-print(database.read()) 
+database.commit()
+print(database.read())
