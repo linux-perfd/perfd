8ea5b5257abc81883037eca54b5a16272ba0753a
Maxim Shcherbakov <marffmarff2@gmail.com>
2022-02-27 11:59:00 +0400

    preparing the profiles for the perf files

--- perfd.py
+++ perfd.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+from heapq import merge
+from multiprocessing.spawn import prepare
 import os
 import tempfile
 import subprocess
 import db
-
+'''
 temp_perf_file = tempfile.NamedTemporaryFile(delete=False)
 # temp_perf_file.name
 
@@ -24,7 +26,8 @@ for line in lines:
         samples_per_obj_file[name] += 1
     else:
         samples_per_obj_file[name] = 1
-
+'''
+samples_per_obj_file = dict() # i'll have to delete this line when i fimish the step 4
 database = db.Database()
 database.table = 'database'
 for name in samples_per_obj_file:
@@ -35,3 +38,20 @@ for name in samples_per_obj_file:
 
 database.commit()
 print(database.read())
+
+def pepare_fdata(objfile):
+    fdata_files = []
+    records = database.read(name_obj_file = objfile)
+    for record in records:
+        fdata_file = tempfile.NamedTemporaryFile(delete=False)
+        perf_name = record[0]
+        os.system(f"perf2bolt {objfile} -p {perf_name} -o {fdata_file.name}")
+        fdata_files.append(fdata_file.name) 
+    merge_fdata = tempfile.NamedTemporaryFile(delete=False)
+    os.system(f"merge-fdata {' '.join(fdata_files)} > {merge_fdata.name}")
+    return merge_fdata.name
+
+
+objfile = '/usr/lib/x86_64-linux-gnu/libresolv-2.31.so'
+pepare_fdata(objfile)
+tempfile.NamedTemporaryFile(delete=True)
