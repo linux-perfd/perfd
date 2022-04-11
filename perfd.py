ffba204c3086a2a6bb1d28f3cfade3b5e0de9b41
Maxim Shcherbakov <marffmarff2@gmail.com>
2022-04-12 01:04:43 +0400

    optimization on excess  the specified number of samples

--- perfd.py
+++ perfd.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from heapq import merge
-from multiprocessing.spawn import prepare
 import os
+import re
 import tempfile
 import subprocess
 import db
-'''
-temp_perf_file = tempfile.NamedTemporaryFile(delete=False)
-# temp_perf_file.name
 
+
+temp_perf_file = tempfile.NamedTemporaryFile(delete=False, dir='.')
+# temp_perf_file.name
+'''
 os.system("perf record -e cycles:u -j any,u -a -o  " +
           temp_perf_file.name + "  -- sleep 10")
 batcmd = "perf script -F ip,dso -f -i " + temp_perf_file.name
@@ -16,6 +16,7 @@ batcmd = "perf script -F ip,dso -f -i " + temp_perf_file.name
 result = subprocess.check_output(batcmd, shell=True, text=True)
 lines = result.splitlines()
 samples_per_obj_file = dict()
+
 
 for line in lines:
     cols = line.split()
@@ -27,31 +28,98 @@ for line in lines:
     else:
         samples_per_obj_file[name] = 1
 '''
-samples_per_obj_file = dict() # i'll have to delete this line when i fimish the step 4
+
+samples_per_obj_file = dict() # i will have to delete it when i finish the step 5
 database = db.Database()
 database.table = 'database'
 for name in samples_per_obj_file:
     database.write(
         perf_name=temp_perf_file.name,
         name_obj_file=name,
-        amount_of_samples=samples_per_obj_file[name])
+        number_of_samples=samples_per_obj_file[name])
 
 database.commit()
-print(database.read())
 
-def pepare_fdata(objfile):
+def prepare_fdata(objfile):
     fdata_files = []
-    records = database.read(name_obj_file = objfile)
+    records = database.read(name_obj_file = objfile) # find some sampels in the db, which have the same name_obj_file
     for record in records:
-        fdata_file = tempfile.NamedTemporaryFile(delete=False)
-        perf_name = record[0]
-        os.system(f"perf2bolt {objfile} -p {perf_name} -o {fdata_file.name}")
-        fdata_files.append(fdata_file.name) 
-    merge_fdata = tempfile.NamedTemporaryFile(delete=False)
-    os.system(f"merge-fdata {' '.join(fdata_files)} > {merge_fdata.name}")
-    return merge_fdata.name
+        fdata_file = tempfile.mktemp()
+        perf_name = record[0] 
+        os.system(f"perf2bolt {objfile} -p {perf_name} -o {fdata_file}") # for every single perf files use perf2bolt
+        fdata_files.append(fdata_file) # add the fdata files in the list  
+    merge_fdata = tempfile.mktemp()
+    os.system(f"merge-fdata {' '.join(fdata_files)} > {merge_fdata}") # merge all fdata files that i got 
+    for fdata_file in fdata_files: 
+        os.remove(fdata_file) # delete the mid-term fdata files and save only a last one
+    return merge_fdata
+
+'''
+objfile = '/usr/lib/x86_64-linux-gnu/gvfs/libgvfscommon.so'
+fdata = prepare_fdata(objfile)
+print(fdata)
+'''
+a= []
+b = dict()
+def invoke_bolt(objfile, fdata_file):
+    outfile = tempfile.mktemp()
+    os.system(f"llvm-bolt {objfile} -data={fdata_file} -o {outfile} -dyno-stats")
+    return outfile 
+records = database.read() #the information's from db.py
+for _, name_obj_file, number_of_samples in records: #looking for obj_files which are larger or equal 100 samples 
+    if name_obj_file in b: #count samples in an every perf_file
+        b[name_obj_file] += number_of_samples
+    else:
+        b[name_obj_file] = number_of_samples 
+for name_obj_file in b:
+    if b[name_obj_file] >= 100: #find obj_files which are more or equel 100 samples
+        if name_obj_file.split('.')[-1] == 'map' and name_obj_file[0] != "/": #skipping lines on certain conditions 
+            a.append(name_obj_file) # add it to the list
+            a.sort(reverse=True) #sort the numbers of samples in decreasing order
+    for perf_name, name_obj_file, number_of_samples in a:
+        fdata_file = tempfile.mktemp()
+        os.system(f"perf2bolt {name_obj_file} -p {number_of_samples} -o {fdata_file}")
+        a.append(fdata_file)
 
 
-objfile = '/usr/lib/x86_64-linux-gnu/libresolv-2.31.so'
-pepare_fdata(objfile)
-tempfile.NamedTemporaryFile(delete=True)
+    
+
+         
+        
+
+
+
+
+
+
+
+#opt_file = invoke_bolt(objfile, fdata)
+#print(opt_file) 
+
+
+
+
+
+
+# probably it will help me on the step 5
+'''
+
+#############################
+
+
+    if file.split('.')[-1] == 'map':
+        
+
+#############################
+
+2)for line in a:
+    if ".map" in line:
+        print(line)
+
+#############################
+
+3)os.system(f"perf2bolt {objfile} -p {...} -o {fdata_file}") #use BOLT for every single obj_files
+
+#############################
+
+'''
